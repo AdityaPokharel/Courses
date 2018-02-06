1e8dec9723648e30c2c0e1182f17714cff2cb497
Aditya Pokharel <pokhareladitya@gmail.com>
2018-02-07 02:08:38 +1100

    minor edit again

--- Udemy/JavaScript/DOM/scorekeeper/script.js
+++ Udemy/JavaScript/DOM/scorekeeper/script.js
@@ -4,6 +4,7 @@ var reset = document.getElementById("reset");
 var display = document.getElementById("display");
 var inp = document.getElementById("inp");
 var label = document.getElementById("label");
+var finalMessage = document.getElementById("finalMessage");
 var p1Score = 0;
 var p2Score = 0;
 var max = 5;
@@ -14,6 +15,7 @@ p1Button.addEventListener("click", function() {
         display.textContent = p1Score + " | " + p2Score;
         if(p1Score == max) {
             display.innerHTML = "<span class=\"green\">" + p1Score + "</span> | " + p2Score;
+            finalMessage.innerText = "Player 1 wins!"
         }
     }
 });
@@ -23,6 +25,7 @@ p2Button.addEventListener("click", function() {
         display.textContent = p1Score + " | " + p2Score;
         if(p2Score == max) {
             display.innerHTML = p1Score + " | <span class=\"green\">" + p2Score + "</span>";
+            finalMessage.innerText = "Player 2 wins!"
         }
     }
 });
@@ -31,6 +34,7 @@ reset.addEventListener("click", function() {
     p1Score = 0;
     p2Score = 0;
     display.textContent = p1Score + " | " + p2Score;
+    finalMessage.innerText = "";
 });
 
 inp.addEventListener("input", function() {
